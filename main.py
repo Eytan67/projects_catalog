5c36fb33a3c95b7ef842a5fd5e8b0bee19326885
Eytan_zichel <eytanzichel@gmail.com>
2025-07-22 15:00:05 +0300

    Initialize database schema creation in main application file

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
 from app.api.endpoints.progects import router as projects_router
+from app.db.database import Base, engine
 
 app = FastAPI(title="Projects Catalog API")
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(projects_router)
 
